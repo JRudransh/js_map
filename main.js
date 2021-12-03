b01678aa6da480ef9663bb37dcfa5aacf994ec04
JRudransh <JRudransh@protonmail.com>
2021-12-03 15:38:28 +0530

    MOD

--- main.js
+++ main.js
@@ -39,6 +39,7 @@ function initMap() {
           infoWindow.setContent("Location found.");
           infoWindow.open(map);
           map.setCenter(pos);
+          map.zoom = 9;
         },
         () => {
           handleLocationError(true, infoWindow, map.getCenter());
