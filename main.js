18d57e06a265e414cbdeb696fd8fb5d284834ab3
JRudransh <JRudransh@protonmail.com>
2021-12-03 15:47:23 +0530

    MOD

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 26.148043, lng: 91.731377 },
     zoom: 5,
+    icon: {
+      url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png",
+      labelOrigin: new google.maps.Point(75, 32),
+      size: new google.maps.Size(32,32),
+      anchor: new google.maps.Point(16,32)
+    }
   });
   lat_c = document.getElementById('lat');
   long_c = document.getElementById('long');
@@ -37,7 +43,7 @@ function initMap() {
 
           infoWindow.setPosition(pos);
           infoWindow.setZoom(9)
-          infoWindow.setContent("Location found.");
+          // infoWindow.setContent("Location found.");
           infoWindow.open(map);
           map.setCenter(pos);
           // map.setZoom(9);
