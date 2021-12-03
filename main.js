ea3cb66573c786033249129d20a35806db761055
JRudransh <JRudransh@protonmail.com>
2021-12-03 17:11:04 +0530

    MOD

--- main.js
+++ main.js
@@ -3,8 +3,9 @@
 // failed.", it means you probably did not give permission for the browser to
 // locate you.
 let map, marker, infoWindow, long_c, lat_c, cords;
-
+coords = { lat: 26.148043, lng: 91.731377 };
 function initMap() {
+  initMap2()
   document.getElementById('button').addEventListener('click', _initMap)
 }
 
@@ -18,7 +19,7 @@ function _initMap() {
       // const myLatLng = { lat: 26.148043, lng: 91.731377 };
       const myLatLng = cords
       map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 7,
+        zoom: 17,
         center: myLatLng,
       });
 
@@ -37,62 +38,25 @@ function _initMap() {
 }
 
 
-// function initMap() {
-//   const myLatLng = { lat: 26.148043, lng: 91.731377 };
-//   map = new google.maps.Map(document.getElementById("map"), {
-//     center: myLatLng,
-//     zoom: 7,
-//   });
-//   marker = new google.maps.Marker({
-//     position: myLatLng,
-//     map,
-//     // title: "Hello World!",
-//   });
+function initMap2() {
+  const myLatLng = coords;
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: myLatLng,
+    zoom: 7,
+  });
+  marker = new google.maps.Marker({
+    position: myLatLng,
+    map,
+    // title: "Hello World!",
+  });
 
-//   function addMarker(location, map) {
-//     new google.maps.Marker({
-//       position: location,
-//       map: map,
-//     });
-//   }
-
-//   lat_c = document.getElementById('lat');
-//   long_c = document.getElementById('long');
-//   lat_c.value = myLatLng.lat;
-//   long_c.value = myLatLng.lng;
-
-//   infoWindow = new google.maps.InfoWindow();
-
-//   const locationButton = document.getElementById("button");
-//   locationButton.addEventListener("click", () => {
-//     // Try HTML5 geolocation.
-//     if (navigator.geolocation) {
-//       navigator.geolocation.getCurrentPosition(
-//         (position) => {
-//           const pos = {
-//             lat: position.coords.latitude,
-//             lng: position.coords.longitude,
-//           };
-
-//           // console.log(pos);
-//           lat_c.value = pos.lat
-//           long_c.value = pos.lng
-//           infoWindow.setPosition(pos);
-//           infoWindow.setContent(null);
-//           infoWindow.open(map);
-//           map.setCenter(pos);
-//           addMarker(pos, map)
-//         },
-//         () => {
-//           handleLocationError(true, infoWindow, map.getCenter());
-//         }
-//       );
-//     } else {
-//       // Browser doesn't support Geolocation
-//       handleLocationError(false, infoWindow, map.getCenter());
-//     }
-//   });
-// }
+  function addMarker(location, map) {
+    new google.maps.Marker({
+      position: location,
+      map: map,
+    });
+  }
+}
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
